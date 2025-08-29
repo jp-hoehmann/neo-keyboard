741143c2c1336fcd85465948f24f4a7b970aa094
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2025-08-29 22:04:44 +0200

    Replace InputSet with KeyboardLayout.InputSet for KeyboardKit 9.9.0.

--- German Neo/NeoKeyboardLayoutService.swift
+++ German Neo/NeoKeyboardLayoutService.swift
@@ -20,31 +20,31 @@ class NeoKeyboardLayoutService: KeyboardLayoutService {
     
     /// The input set to use for alphanumeric keyboards.
     ///
-    public static var alphabeticInputSet: InputSet {
-        InputSet(rows: [
-            InputSet.ItemRow(chars: "xvlcwkhgfqß"),
-            InputSet.ItemRow(chars: "uiaeosnrtdy"),
-            InputSet.ItemRow(lowercased: "üöäpzbm,.j", uppercased: "ÜÖÄPZBM–•J")
+    public static var alphabeticInputSet: KeyboardLayout.InputSet {
+        KeyboardLayout.InputSet(rows: [
+            KeyboardLayout.InputSet.ItemRow(chars: "xvlcwkhgfqß"),
+            KeyboardLayout.InputSet.ItemRow(chars: "uiaeosnrtdy"),
+            KeyboardLayout.InputSet.ItemRow(lowercased: "üöäpzbm,.j", uppercased: "ÜÖÄPZBM–•J")
         ])
     }
     
     /// The input set to use for numeric keyboards.
     ///
-    public static var numericInputSet: InputSet {
-        InputSet(rows: [
-            InputSet.ItemRow(chars: "1234567890'"),
-            InputSet.ItemRow(chars: "\\/{}*?()-:@"),
-            InputSet.ItemRow(chars: "°§ℓ»«$€„“”"),
+    public static var numericInputSet: KeyboardLayout.InputSet {
+        KeyboardLayout.InputSet(rows: [
+            KeyboardLayout.InputSet.ItemRow(chars: "1234567890'"),
+            KeyboardLayout.InputSet.ItemRow(chars: "\\/{}*?()-:@"),
+            KeyboardLayout.InputSet.ItemRow(chars: "°§ℓ»«$€„“”"),
         ])
     }
     
     /// The input set to use for symbolic keyboards.
     ///
-    public static var symbolicInputSet: InputSet {
-        InputSet(rows: [
-            InputSet.ItemRow(chars: "#$|~`+%\"'; "),
-            InputSet.ItemRow(chars: "…_[]^!<>=&ſ"),
-            InputSet.ItemRow(chars: "¹²³›‹¢¥‚‘’")
+    public static var symbolicInputSet: KeyboardLayout.InputSet {
+        KeyboardLayout.InputSet(rows: [
+            KeyboardLayout.InputSet.ItemRow(chars: "#$|~`+%\"'; "),
+            KeyboardLayout.InputSet.ItemRow(chars: "…_[]^!<>=&ſ"),
+            KeyboardLayout.InputSet.ItemRow(chars: "¹²³›‹¢¥‚‘’")
         ])
     }
     
